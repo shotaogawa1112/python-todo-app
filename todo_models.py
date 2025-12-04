455608de8fb6fad1c04df047e291b55f9a833968
shotaogawa1112 <shota.ogawa@iterra.co.jp>
2025-12-04 14:48:21 +0900

    sample実行用

---
+++ todo_models.py
@@ -0,0 +1,11 @@
+class Todo:
+    def __init__(self,id:int,title:str,done=False):
+        self.id = id
+        self.title = title
+        self.done = done
+
+    def __repr__(self):
+        return f"Todo(id={self.id}, title='{self.title}', done={self.done})"
+    
+sample = Todo(10, "山田", True)
+print(sample)
